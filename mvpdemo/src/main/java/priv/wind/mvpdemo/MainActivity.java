ce47adecf083c09955500dc2adcac42113cb1f17
windofire <windofire@163.com>
2017-12-20 10:03:40 +0800

    第一次在AS提交到GIT

--- mvpdemo/src/main/java/priv/wind/mvpdemo/MainActivity.java
+++ mvpdemo/src/main/java/priv/wind/mvpdemo/MainActivity.java
@@ -2,12 +2,15 @@ package priv.wind.mvpdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnOne;//按钮一
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
